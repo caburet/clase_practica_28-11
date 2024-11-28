8e26134b268ac9f2881012f151819f5fd50addfb
enedamian <142589183+enedamian@users.noreply.github.com>
2024-11-28 19:26:02 -0300

    first commit

---
+++ modelos/entidades/tarea.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+
+class Tarea:
+    # atributo de clase
+    __ultimo_id = 0
+
+    @classmethod
+    def generar_siguiente_id(cls)->int:
+        cls.__ultimo_id += 1
+        return cls.__ultimo_id
+    
+    @classmethod
+    def establecer_ultimo_id(cls, ultimo_id:int):
+        if not isinstance(ultimo_id, int) or ultimo_id < 0:
+            raise ValueError(f'El último id debe ser un entero positivo')
+        cls.__ultimo_id = ultimo_id
+
+    @classmethod
+    def obtener_ultimo_id(cls)->int:
+        return cls.__ultimo_id
+
+    @classmethod
+    def fromDiccionario(cls, diccionario:dict):
+        if not isinstance(diccionario, dict):
+            raise ValueError(f'El diccionario debe ser de tipo dict')
+        if "id" in diccionario and "fecha_creacion" in diccionario:
+            return cls(diccionario['titulo'], diccionario['descripcion'], diccionario['completada'], diccionario['id'], diccionario['fecha_creacion'])
+        else:
+            return cls(diccionario['titulo'], diccionario['descripcion'], diccionario['completada'])
+
+
+    def __init__(self, titulo:str, descripcion:str, completada:bool, id:int = None, fecha_creacion:str = None):
+        if not isinstance(titulo, str) or titulo == "" or titulo.isspace():
+            raise ValueError(f'El titulo debe ser de tipo string y no estar vacío')
+        if not isinstance(descripcion, str) or descripcion == "" or descripcion.isspace():
+            raise ValueError(f'La descripción debe ser de tipo string y no estar vacía')
+        if not isinstance(completada, bool):
+            raise ValueError(f'El campo completada debe ser de tipo booleano')
+        if id is not None and not isinstance(id, int) or id < 0:
+            raise ValueError(f'El id debe ser un entero positivo')
+        if fecha_creacion is not None and not isinstance(fecha_creacion, str) or fecha_creacion == "":
+            raise ValueError(f'La fecha de creación debe ser un string y no estar vacía')
+        
+        self.__titulo = titulo
+        self.__descripcion = descripcion
+        self.__completada = completada
+        
+        if id is not None:
+            self.__id = id
+        else:
+            self.__id = Tarea.generar_siguiente_id()
+        
+        if fecha_creacion is not None:
+            self.__fecha_creacion = fecha_creacion
+        else:
+            self.__fecha_creacion = datetime.strptime(datetime.now(), '%Y-%m-%d')
+    
+    def establecer_titulo(self, titulo:str):
+        if not isinstance(titulo, str) or titulo == "" or titulo.isspace():
+            raise ValueError(f'El titulo debe ser de tipo string y no estar vacío')
+        self.__titulo = titulo
+
+    def establecer_descripcion(self, descripcion:str):
+        if not isinstance(descripcion, str) or descripcion == "" or descripcion.isspace():
+            raise ValueError(f'La descripción debe ser de tipo string y no estar vacía')
+        self.__descripcion = descripcion
+
+    def establecer_completada(self, completada:bool):
+        if not isinstance(completada, bool):
+            raise ValueError(f'El campo completada debe ser de tipo booleano')
+        self.__completada = completada
+
+    def obtener_titulo(self)->str:
+        return self.__titulo
+    
+    def obtener_descripcion(self)->str:
+        return self.__descripcion
+    
+    def obtener_completada(self)->bool:
+        return self.__completada
+    
+    def obtener_id(self)->int:
+        return self.__id
+    
+    def obtener_fecha_creacion(self):
+        return self.__fecha_creacion
+    
+    def toDiccionario(self):
+        return {
+            "id": self.__id,
+            "titulo": self.__titulo,
+            "descripcion": self.__descripcion,
+            "completada": self.__completada,
+            "fecha_creacion": self.__fecha_creacion
+        }
---
+++ modelos/repositorios/repositorio_tareas.py
@@ -0,0 +1,91 @@
+from modelos.entidades.tarea import Tarea
+import json
+
+class RepositorioTareas:
+    #atributo de clase
+    __ruta_archivo = 'datos/tareas.json'
+
+    def __init__(self):
+        self.__tareas = []
+        self.__cargarDatos()
+
+    def __cargarDatos(self):
+        try:
+            with open(RepositorioTareas.__ruta_archivo, 'r', encoding="utf8") as archivo:
+                dicc_guardado = json.load(archivo)
+                Tarea.establecer_ultimo_id(dicc_guardado['ultimo_id'])
+                lista_datos = dicc_guardado['tareas']
+                for tarea in lista_datos:
+                    self.__tareas.append(Tarea.fromDiccionario(tarea))
+        except FileNotFoundError:
+            print(f'No se encontró el archivo tareas.json')
+
+    def __guardarDatos(self):
+        try:
+            with open(RepositorioTareas.__ruta_archivo, 'w', encoding="utf8") as archivo:
+                lista_datos = []
+                for tarea in self.__tareas:
+                    lista_datos.append(tarea.toDiccionario())
+
+                dicc_a_guardar = {
+                    "ultimo_id": Tarea.obtener_ultimo_id(),
+                    "tareas": lista_datos
+                }                
+                json.dump(dicc_a_guardar, archivo, indent=4)
+        except FileNotFoundError:
+            print(f'No se encontró el archivo tareas.json')
+
+    def obtenerTodas(self):
+        return self.__tareas
+    
+    def obtenerPorId(self, id:int)->Tarea:
+        if not isinstance(id, int) or id < 0:
+            raise ValueError(f'El id debe ser un entero positivo')
+        for tarea in self.__tareas:
+            if isinstance(tarea, Tarea) :
+                if tarea.obtener_id() == id:
+                    return tarea
+        return None
+    
+    def existeTarea(self, titulo:str)->bool:
+        if not isinstance(titulo, str) or titulo == "" or titulo.isspace():
+            raise ValueError(f'El titulo debe ser de tipo string y no estar vacío')
+        for tarea in self.__tareas:            
+            if tarea.obtener_titulo() == titulo:
+                return True
+        return False
+
+    def agregar(self, tarea:Tarea):
+        if not isinstance(tarea, Tarea):
+            raise ValueError(f'El objeto tarea debe ser de tipo Tarea')
+        if not self.existeTarea(tarea.obtener_titulo()):
+            self.__tareas.append(tarea)
+            self.__guardarDatos()
+    
+    def actualizar(self, id: int, dicc: dict):
+        if not isinstance(dicc, dict):
+            raise ValueError(f'El diccionario debe ser de tipo dict')
+        if not isinstance(id, int) or id < 0:
+            raise ValueError(f'El id debe ser un entero positivo')
+        tarea = self.obtenerPorId(id)
+        if tarea is not None:
+            if "titulo" in dicc:
+                tarea.establecer_titulo(dicc['titulo'])
+            if "descripcion" in dicc:
+                tarea.establecer_descripcion(dicc['descripcion'])
+            if "completada" in dicc:
+                tarea.establecer_completada(dicc['completada'])
+            self.__guardarDatos()
+            
+        return tarea #aca puede ser None o un objeto de Tarea con los datos actualizados
+    
+    def eliminar(self, id:int):
+        if not isinstance(id, int) or id < 0:
+            raise ValueError(f'El id debe ser un entero positivo')
+        for t in self.__tareas:
+            if t.obtener_id() == id:
+                self.__tareas.remove(t)
+                self.__guardarDatos()
+                return True
+        return False
+    
